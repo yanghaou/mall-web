0b9fb52b24664a40b47f062e355431944c496d99
牛健 <mohatie@126.com>
2019-05-15 17:28:37 +0800

    结构调整

---
+++ web-common/src/main/java/com/mall/common/util/PageInfoUtil.java
@@ -0,0 +1,24 @@
+package com.mall.common.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * author :y.hao
+ * time :2019/4/10
+ * function:
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageInfoUtil<T> {
+    @NotNull
+    private Integer page;
+    @NotNull
+    private Integer pageSize;
+    T Info;
+
+}
